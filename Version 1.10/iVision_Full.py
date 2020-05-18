78d8aee5da2e2bf774de23b06dad5840831cc0a1
HarirajRajkumar <harirajkumar1997@gmail.com>
2020-05-18 22:10:43 +0530

    Version 1.10 Full version

--- Version 1.10/iVision_Full.py
+++ Version 1.10/iVision_Full.py
@@ -1,3 +1,5 @@
+# Full version 1.10
+
 from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QMetaObject,
     QObject, QPoint, QRect, QSize, QTime, QUrl, Qt)
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
